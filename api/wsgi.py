20b5c2df7f6d1ecd2197cf1f30baab1ce9114c02
username <sample@example.com>
2022-07-12 15:35:54 -0600

    Fix: Deploy Issues

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,8 @@
+import os
+
 from src.app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False)
+    app.run(host="0.0.0.0", debug=False, port=os.environ.get("PORT"))
