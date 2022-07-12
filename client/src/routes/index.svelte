714bc0f56b9346e2833db8c08a9dd0c086aa199e
username <sample@example.com>
2022-07-12 15:45:19 -0600

    Fix: Service URL

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -3,7 +3,9 @@
 	import type { LoadEvent } from '@sveltejs/kit';
 
 	export async function load({ fetch }: LoadEvent) {
-		const response = await fetch('http://127.0.0.1:5000/api/items?page=1&page_id=NDk=');
+		const response = await fetch(
+			`${import.meta.env.VITE_ETRNAL_SERVICE}/api/items?page=1&page_id=NDk=`
+		);
 
 		if (response.ok) {
 			const { page_id, records } = (await response.json()) as DataIn;
